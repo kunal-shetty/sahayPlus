84a79967dc57c6f7665b6039b7ac99c8b9a22d28
kunal-shetty <kunalshetty567@gmail.com>
2026-09-06 22:44:21 +0530

    refactor(hooks): improve responsive device detection

--- hooks/use-mobile.ts
+++ hooks/use-mobile.ts
@@ -1,7 +1,19 @@
+/**
+ * @file use-mobile.ts
+ * @description Hook for detecting whether the current viewport is in mobile size.
+ */
+
 import * as React from 'react'
 
+/** The breakpoint width in pixels below which the device is considered mobile. */
 const MOBILE_BREAKPOINT = 768
 
+/**
+ * Custom hook that returns a boolean indicating if the user is on a mobile device
+ * based on the screen width.
+ *
+ * @returns {boolean} True if the screen width is below the MOBILE_BREAKPOINT.
+ */
 export function useIsMobile() {
   const [isMobile, setIsMobile] = React.useState<boolean | undefined>(undefined)
 
